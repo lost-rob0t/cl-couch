b5c659e1e8d3efb7f9ad508f61ada4541fd900b9
nsaspy <nsaspy@airmail.cc>
2023-05-31 15:28:05 -0400

    add files

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Search CVE from the command line and a web app";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShell.x86_64-linux =
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkg-config
+          roswell
+          sbcl
+          # normally stuff goes in here
+        ];
+        shellHook = ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath([pkgs.openssl])}
+            '';
+      };
+  };
+}
